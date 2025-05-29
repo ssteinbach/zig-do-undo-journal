a92fb86e12655f8a0ed607077c50cbf2a6b6e0d1
ssteinbach <ssteinbach@users.noreply.github.com>
2025-05-28 22:11:13 -0700

    add redo system

--- build.zig
+++ build.zig
@@ -16,10 +16,11 @@ pub fn build(
         }
     );
 
+    const name = "zig_do_undo_journal";
     const lib = b.addLibrary(
         .{
             .linkage = .static,
-            .name = "zig_do_undo_journal",
+            .name = name,
             .root_module = lib_mod,
         }
     );
@@ -28,7 +29,10 @@ pub fn build(
 
     const lib_unit_tests = b.addTest(
         .{
+            .name = "test_" ++ name,
             .root_module = lib_mod,
+            .optimize = optimize,
+            .target = target,
         }
     );
 
@@ -36,4 +40,11 @@ pub fn build(
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    // also install the test binary for lldb needs
+    const install_test_bin = b.addInstallArtifact(
+        lib_unit_tests,
+        .{},
+    );
+    test_step.dependOn(&install_test_bin.step);
 }
--- src/journal.zig
+++ src/journal.zig
@@ -6,11 +6,20 @@ const command = @import("command.zig");
 
 /// a journal of commands that support undo/redo
 const Journal = struct {
+    allocator: std.mem.Allocator,
+
     /// sorted such that lower indices in the entries arraylist are earlier
     /// than entries with larger indices
     entries: std.ArrayList(command.Command),
+
+    /// limit on the number of entries.  Entries added once the Journal is at
+    /// max_depth entries will cause the first entries in the journal to be
+    /// removed.
     max_depth: usize,
-    allocator: std.mem.Allocator,
+
+    /// the current head.  Undo will move this back from most recently added 
+    /// entry in the .entries ArrayList
+    maybe_head_entry: ?usize,
 
     pub fn init(
         allocator: std.mem.Allocator,
@@ -24,7 +33,16 @@ const Journal = struct {
             .allocator = allocator,
             .entries = entries,
             .max_depth = max_depth,
+            .maybe_head_entry = null,
         };
+    }
+
+    /// returns whether this journal has any entries that can be undone
+    pub fn can_undo(
+        self: @This(),
+    ) bool
+    {
+        return self.maybe_head_entry != null;
     }
 
     /// add a command to the end of the journal
@@ -33,12 +51,27 @@ const Journal = struct {
         cmd: command.Command,
     ) !void
     {
+        if (self.maybe_head_entry)
+            |head_index|
+        {
+            self.truncate(head_index);
+        }
+
         try self.entries.append(cmd);
 
+        // if the journal was full
         if (self.entries.items.len > self.max_depth) 
         {
             var popped_thing = self.entries.orderedRemove(0);
             popped_thing.destroy(self.allocator);
+        }
+        // if it wasn't, increment the head index pointer
+        else
+        {
+            self.maybe_head_entry = (
+                if (self.maybe_head_entry == null) 0 
+                else (self.maybe_head_entry.? + 1)
+            );
         }
     }
 
@@ -47,13 +80,84 @@ const Journal = struct {
          self: *@This(),
      ) !void
      {
-         if (self.entries.items.len == 0) {
+         // nothing else to undo
+         if (self.entries.items.len == 0 or self.maybe_head_entry == null) 
+         {
              return;
          }
 
-         const cmd = self.entries.pop().?;
-         try cmd.undo();
-         cmd.destroy(self.allocator);
+         const head_index = self.maybe_head_entry.?;
+
+         try self.entries.items[head_index].undo();
+
+         self.maybe_head_entry = (
+             if (head_index > 0) head_index - 1
+             else null
+         );
+     }
+
+     pub fn redo(
+         self: *@This(),
+     ) !void
+     {
+         // nothing to redo
+         if (self.maybe_head_entry)
+             |index|
+         {
+             if (index >= self.entries.items.len - 1)
+             {
+                 self.maybe_head_entry = self.entries.items.len - 1;
+                 return;
+             }
+
+             const next_index = index + 1;
+             try self.entries.items[next_index].do();
+             self.maybe_head_entry = next_index;
+         }
+     }
+
+     /// clears the history from the end until the given index is reached.
+     /// For example, if the Journal includes 5 entries, and 2 is passed as the
+     /// until_index, then it will remove the [4] and the [3] entry but leave
+     /// the [2] entry.
+     pub fn truncate(
+         self: *@This(),
+         maybe_index: ?usize,
+     ) void
+     {
+         if (maybe_index)
+             |index|
+         {
+             if (index >= self.entries.items.len)
+             {
+                 return;
+             }
+
+             while (self.entries.items.len - 1 > index)
+             {
+                 const cmd = self.entries.pop().?;
+                 cmd.destroy(self.allocator);
+             }
+
+             self.maybe_head_entry = index;
+         }
+         else
+         {
+             self.clear();
+         }
+     }
+
+     /// free all entries in the journal
+     pub fn clear(
+         self: *@This(),
+     ) void
+     {
+         while (self.entries.items.len > 0)
+         {
+             const cmd = self.entries.pop().?;
+             cmd.destroy(self.allocator);
+             self.maybe_head_entry = null;
+         }
      }
 
     pub fn deinit(
@@ -106,11 +210,13 @@ test "Journal Test"
         try journal.add(cmd);
         try std.testing.expectEqual(TEST_JOURNAL_LIMIT, journal.max_depth);
         try std.testing.expectEqual(1, journal.entries.items.len);
+        try std.testing.expectEqual(0, journal.maybe_head_entry);
     }
 
     try journal.undo();
     try std.testing.expectEqual(TEST_JOURNAL_LIMIT, journal.max_depth);
-    try std.testing.expectEqual(0, journal.entries.items.len);
+    try std.testing.expectEqual(1, journal.entries.items.len);
+    try std.testing.expectEqual(null, journal.maybe_head_entry);
 
     var i:TEST_TYPE = 1;
     while (i <= 5)
@@ -134,7 +240,7 @@ test "Journal Test"
     try std.testing.expectEqual(TEST_JOURNAL_LIMIT, journal.max_depth);
     try std.testing.expectEqual(TEST_JOURNAL_LIMIT, journal.entries.items.len);
 
-    while (journal.entries.items.len > 0)
+    while (journal.can_undo())
         : (try journal.undo())
     {
     }
@@ -142,7 +248,8 @@ test "Journal Test"
     try std.testing.expectEqual(2, value);
 
     try std.testing.expectEqual(TEST_JOURNAL_LIMIT, journal.max_depth);
-    try std.testing.expectEqual(0, journal.entries.items.len);
+    try std.testing.expectEqual(3, journal.entries.items.len);
+    try std.testing.expectEqual(null, journal.maybe_head_entry);
 
 }
 
@@ -188,7 +295,8 @@ test "Journal Test (undo/redo)"
     try std.testing.expectEqual(3, value);
 
     try std.testing.expectEqual(TEST_JOURNAL_LIMIT, journal.max_depth);
-    try std.testing.expectEqual(1, journal.entries.items.len);
+    try std.testing.expectEqual(3, journal.entries.items.len);
+    try std.testing.expectEqual(0, journal.maybe_head_entry);
 
     try journal.redo();
 }
